b4337322e416fdafe259603c7dd4d4fa25790a4a
sallecta <sallecta@inbox.ru>
2023-08-07 17:43:35 +0300

    basepath, examples in doc dir, stylesheets loading in main script

--- client_ui/client_ui.js
+++ client_ui/client_ui.js
@@ -2,23 +2,62 @@
 const client_ui = {};
 client_ui.version = '0.1.0';
 client_ui.name = 'client_ui';
-client_ui.modules_id = ['core','gallery'];
+client_ui.resources = ['core','gallery'];
+client_ui.resources = [];
+client_ui.resources.push({type:'css',id:'client_ui',});
+client_ui.resources.push({type:'module',id:'core',});
+client_ui.resources.push({type:'module',id:'gallery',});
 client_ui.mdls = {};
 client_ui.load_err = false;
 client_ui.events = {};
+client_ui.dirname = function( a_path,a_levels=1 )
+{
+	const me=client_ui.name+'.dirname';
+	let path_len = a_path.length;
+	let out;
+	let level=1;
+	for (let ndx = path_len-1; ndx > -1; ndx--)
+	{
+		if ( a_path.charAt(ndx-1) === '/')
+		{
+			if ( level === a_levels )
+			{
+				//console.log('level === a_levels',level,a_levels);
+				out = a_path.slice(0,ndx-1);
+				break;
+			}
+			//console.log('level != a_levels',level,a_levels);
+			level = level + 1;
+		}
+	}
+	return out;
+} // client_ui.dirname
+
+client_ui.path = client_ui.dirname(window.document.currentScript.src);
 
 client_ui.includer = function()
 {
 	const me = 'client_ui.includer';
 	var cnt = 0;
-	function add_script(a_id, a_fn)
+	function add_resource(a_res, a_fn)
 	{
-		const me = 'client_ui.includer.add_script';
-		const el = document.createElement('script');
-		el.id = a_id;
-		el.src = 'client_ui/modules/'+a_id+'/'+a_id+'.js'; 
-		el.type = 'text/javascript';
-		el.async = true;
+		const me = 'client_ui.includer.add_resource';
+		let el;
+		const id = a_res.id;
+		if ( a_res.type === 'module' )
+		{
+			el = document.createElement('script');
+			el.src = client_ui.path+'/modules/'+id+'/'+id+'.js'; 
+			el.type = 'text/javascript';
+		}
+		if ( a_res.type === 'css' )
+		{
+			el = document.createElement('link');
+			el.rel = 'stylesheet'; 
+			el.href = client_ui.path+'/css/'+id+'.css'; 
+			el.type = 'text/css';
+		}
+		el.id = id;
 		el.addEventListener('error',a_fn);
 		el.addEventListener('load',a_fn);
 		document.head.appendChild(el);
@@ -29,20 +68,19 @@ client_ui.includer = function()
 	client_ui.events.ready.name = 'client_ui.ready';
 	client_ui.events.ready.initEvent(client_ui.events.ready.name, true, true);
 	
-	
-	//console.log( me,cnt,' loading module',client_ui.modules_id[cnt] );
-	add_script(client_ui.modules_id[cnt],next);
+	//console.log( me,cnt,' loading resource',client_ui.resources[cnt] );
+	add_resource(client_ui.resources[cnt],next);
 	
 	function next(a_ev)
 	{
 		cnt = cnt + 1;
 		if ( a_ev.type === 'load')
 		{
-			//console.log(me,cnt,' module',a_ev.target.id,'loaded');
-			if ( client_ui.modules_id[cnt] )
+			//console.log(me,cnt,' resource',a_ev.target.id,'loaded');
+			if ( client_ui.resources[cnt] )
 			{
-				//console.log( me,cnt,' loading module',client_ui.modules_id[cnt] );
-				add_script(client_ui.modules_id[cnt],next);
+				//console.log( me,cnt,' loading resource',client_ui.resources[cnt] );
+				add_resource(client_ui.resources[cnt],next);
 			}
 			else
 			{
@@ -54,12 +92,12 @@ client_ui.includer = function()
 		else if ( a_ev.type === 'error' )
 		{
 			client_ui.load_err = true;
-			console.error(me,cnt,' module',a_ev.target.id,'not loaded');
+			console.error(me,cnt,' resource',a_ev.target.id,'of type',a_ev.target.type,'not loaded');
 		}
 		else
 		{
 			client_ui.load_err = true;
-			console.error(me,cnt,'unknown result on module',a_ev.id,a_ev.type);
+			console.error(me,cnt,'unknown result on resource',a_ev.id,a_ev.type);
 		}
 		
 	};
